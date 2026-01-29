35c2df22379a3b9cf049269ead2cdaa57bf4427a
César Pérez <ca.andradeperez@gmail.com>
2026-01-29 01:26:38 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,7 +61,7 @@ ROOT_URLCONF = 'blango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'blango' / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-from blog.models import Post
 from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from blog.models import Post
+from blog.forms import CommentForm
 
 class PostList(ListView):
     model = Post
@@ -13,3 +13,27 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
+    template_name = "blog/post_detail.html"
+
+    def get(self, request, slug):
+        post = get_object_or_404(self.model, slug=slug)
+        if request.user.is_active:
+            comment_form = CommentForm()
+        else:
+            comment_form = None
+        return render(request, self.template_name, {"post": post, "comment_form": comment_form})
+    
+    def post(self, request, slug):
+        post = get_object_or_404(self.model, slug=slug)
+        if request.user.is_active:
+            comment_form = CommentForm(request.POST)
+
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.content_object = post
+                comment.creator = request.user
+                comment.save()
+                return redirect(request.path_info)
+        else:
+            comment_form = None
+        return render(request, self.template_name, {"post": post, "comment_form": comment_form})
