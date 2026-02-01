054468208777f5a4522ba8ce2de2e51412e7547e
César Pérez <ca.andradeperez@gmail.com>
2026-02-01 22:10:50 +0000

    Finish viewsets and routers

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from blog.models import Post, Tag, Comment
 from blango_auth.models import User
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
+        
 class PostSerializer(serializers.ModelSerializer):
     tags = serializers.SlugRelatedField(
         slug_field="value", many=True, queryset=Tag.objects.all()
